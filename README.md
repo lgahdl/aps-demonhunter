4f7b2d4948ea96aa552f9d180ba67ac4e31bc976
luizgustavoah <luizgustavoahsc@gmail.com>
2021-03-07 17:51:43 -0300

    first commit;

---
+++ README.md
@@ -0,0 +1,14 @@
+##Developer
+Java Version: 11.0.6
+
+### Get Started
+To start the game you will need to: <br/>
+1 - Install Java SE 11 [(available here)](https://jdk.java.net/java-se-ri/11) <br/>
+2 - Open the project in your favorite IDE; <br/>
+3 - Build and Run
+
+## Game
+
+This game is about a demon hunter that is surviving in a dangerous world. <br/>
+Kill your enemies in randomly generated maps and pass the levels, or die in <br/>
+the hands of the most fearsome creatures from the underworld.
---
+++ src/br/com/demonhunter/main/Game.java
@@ -0,0 +1,168 @@
+package br.com.demonhunter.main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+public class Game extends Canvas implements Runnable, KeyListener, MouseListener, MouseMotionListener {
+
+    private static final long serialVersionUID = 1L;
+    public static JFrame frame;
+    private Thread thread;
+    private boolean isRunning;
+    private static BufferedImage image;
+
+    private final static int WIDTH = 420;
+    private final static int HEIGHT = 237;
+    public final static int SCALE = 3;
+
+    public Game(){
+
+        addKeyListener(this);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        initFrame();
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    }
+
+    private void initFrame() {
+        frame = new JFrame("Game");
+        frame.add(this);
+        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+
+    public void tick() {
+
+    }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = image.getGraphics();
+
+
+        g.setColor(new Color(25, 10, 100));
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        g.dispose();
+        g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+        bs.show();
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        @SuppressWarnings("unused")
+        int frames = 0;
+        double timer = System.currentTimeMillis();
+        requestFocus();
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+
+            if (System.currentTimeMillis() - timer >= 1000) {
+//				System.out.println(frames);
+                frames = 0;
+                timer += 1000;
+            }
+        }
+
+        stop();
+    }
+
+    public synchronized void stop() {
+
+        isRunning = false;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
